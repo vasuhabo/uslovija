ac27a22f2855a2d1f48b027339227fc1c00a1e4b
ilia <7523460@gmail.com>
2021-08-09 01:22:43 +0300

    switch  uslovije

--- script.js
+++ script.js
@@ -7,12 +7,48 @@ if (4 == 6) {
    console.log('errorrrr'); 
 }
 
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Error');
+// }  else if (num > 100) {
+//     console.log('mnogo');
+// } else {
+//     console.log('ok!');
+// }
+
+// (num === 50) ? console.log('ok!') : console.log('Error');
+
+
+
+
 const num = 50;
 
-if (num < 49) {
-    console.log('Error');
-}  else if (num > 100) {
-    console.log('mnogo');
-} else {
-    console.log('ok!');
+switch (num) {
+case 49:
+    console.log('neverno');
+    break;
+    case 100:
+        console.log('neverno');
+        break;
+       case 540:
+           console.log('v tochku');
+           break;  
+           default:
+               console.log('ne v etot raz');
+               break; 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
